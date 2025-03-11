d94cfddb4a807699280ce95e97ccb8e5b210c58f
younz <younas1996@live.dk>
2025-03-11 11:06:25 +0100

    [project started]

--- index.php
+++ index.php
@@ -9,5 +9,13 @@ include_once ROOT_PATH . '/public/nav.php';
 ?>
     <main>
         <p>At the forefront of progress, we shape possibilities into realities through a seamless fusion of expertise, adaptability, and forward-thinking solutions. Driven by an unwavering commitment to excellence, we navigate complexity with precision, delivering results that redefine expectations. Whether optimizing existing frameworks or pioneering new frontiers, our approach remains dynamic, innovative, and tailored to the ever-evolving landscape.</p>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
     </main>
 <?php include_once ROOT_PATH . '/public/footer.php'; ?>
